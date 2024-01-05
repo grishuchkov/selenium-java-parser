8da9eaab9214915c304aace06acd1c7c09182c21
grishuchkov <gri.d.a@ya.ru>
2024-01-05 16:05:52 +0300

    chore refactoring

--- src/main/java/ru/grishuchkov/seleniumparser/service/ParseServiceImpl.java
+++ src/main/java/ru/grishuchkov/seleniumparser/service/ParseServiceImpl.java
@@ -2,17 +2,15 @@ package ru.grishuchkov.seleniumparser.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.grishuchkov.seleniumparser.dto.Report;
 import ru.grishuchkov.seleniumparser.utils.StringUtils;
+import ru.grishuchkov.seleniumparser.utils.TimeUtils;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
@@ -20,15 +18,29 @@ import java.time.format.DateTimeFormatter;
 public class ParseServiceImpl implements ParseService {
 
     private final WebDriver webDriver;
-    private final StringUtils utils;
+    private final StringUtils stringUtils;
+    private final TimeUtils timeUtils;
+
 
     @Value("${site.people-class}")
     private String classNameWithPeopleCounter;
 
+    @Override
+    public Report parse(String url) {
+        Report report = new Report();
+        try {
+            log.info("Start parse: " + url);
+            report = doParse(url);
+
+            log.info(String.valueOf(report));
+            log.info("Stop parse");
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+        }
+        return report;
+    }
 
     private Report doParse(String siteUrl) {
-        log.info("Start parse: " + siteUrl);
-
         webDriver.get(siteUrl);
 
         webDriver.manage()
@@ -39,34 +51,9 @@ public class ParseServiceImpl implements ParseService {
                 .findElement(By.className(classNameWithPeopleCounter))
                 .getText();
 
+        int peopleCount = stringUtils
+                .getPeopleCount(stringWithCount);
 
-        int peopleCount = utils.getPeopleCount(stringWithCount);
-
-        log.info("Stop parse");
-
-        Report report = new Report(peopleCount, getDate());
-        log.info(String.valueOf(report));
-
-        return report;
+        return new Report(peopleCount, timeUtils.getCurrentDateAndTime());
     }
-
-    @Override
-    public Report parse(String url) {
-        Report report = new Report();
-        try {
-            report = doParse(url);
-        } catch (Exception exception) {
-            log.error(exception.getMessage());
-        }
-        return report;
-    }
-
-    @NotNull
-    private String getDate() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
-        LocalDateTime localDateTime = LocalDateTime.now();
-        return localDateTime.format(formatter);
-    }
-
-
 }
